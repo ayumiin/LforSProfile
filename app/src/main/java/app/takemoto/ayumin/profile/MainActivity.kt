3cff3eecbdb95ca663505206e33e8b775b8322b8
ayumiin <rsamuv77@gmail.com>
2020-09-07 05:08:33 +0900

    profile

--- app/src/main/java/app/takemoto/ayumin/profile/MainActivity.kt
+++ app/src/main/java/app/takemoto/ayumin/profile/MainActivity.kt
@@ -1,5 +1,7 @@
 package app.takemoto.ayumin.profile
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.isVisible
@@ -17,6 +19,14 @@ class MainActivity : AppCompatActivity() {
             profileImage.setImageResource(R.drawable.randy_image)
             //ラベルをTextViewに反映させる
             profileLabelText.text = "関西メンターのあゆみんです！"
+
+            //押されたボタンの色を変える
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(0,180,220))
+
+            //他のボタンの色を変える
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
         }
 
         //スポーツのボタンがタップされた時
@@ -28,6 +38,14 @@ class MainActivity : AppCompatActivity() {
             profileLabelText.text = "スポーツ"
             //コメントをTextViewに反映する
             profileCommentText.text = "好きな動物はネコです！"
+
+            //押されたボタンの色を変える
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(220,0,100))
+
+            //他のボタンの色を変える
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
         }
 
         //スポーツのボタンがタップされた時
@@ -39,6 +57,14 @@ class MainActivity : AppCompatActivity() {
             profileLabelText.text = "好きな食べ物"
             //コメントをTextViewに反映する
             profileCommentText.text = "甘いものが好きです"
+
+            //押されたボタンの色を変える
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(0,180,120))
+
+            //他のボタンの色を変える
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
         }
 
         //スポーツのボタンがタップされた時
@@ -50,6 +76,14 @@ class MainActivity : AppCompatActivity() {
             profileLabelText.text = "趣味"
             //コメントをTextViewに反映する
             profileCommentText.text = "趣味はゲームです"
+
+            //押されたボタンの色を変える
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(220,100,0))
+
+            //他のボタンの色を変える
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
         }
     }
 }
