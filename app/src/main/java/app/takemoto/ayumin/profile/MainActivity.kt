785fc58a35f07d945ccb0d8482957d7de55781c2
ayumiin <rsamuv77@gmail.com>
2020-09-07 04:42:11 +0900

    profile

--- app/src/main/java/app/takemoto/ayumin/profile/MainActivity.kt
+++ app/src/main/java/app/takemoto/ayumin/profile/MainActivity.kt
@@ -10,18 +10,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //画像を見えないようにする
-        profileImage.isVisible = false
-
-        //テキストを見えないようにする
-        profileCommentText.isVisible = false
-
+        //名前ボタンがタップされた時
         button1.setOnClickListener{
 
-            //画像を見える状態にする
-            profileImage.isVisible = true
+            //画像をimageViewに反映する
+            profileImage.setImageResource(R.drawable.randy_image)
+            //ラベルをTextViewに反映させる
+            profileLabelText.text = "関西メンターのあゆみんです！"
+        }
 
-            profileCommentText.isVisible = true
+        //スポーツのボタンがタップされた時
+        button2.setOnClickListener{
+
+            //画像をimageViewに反映する
+            profileImage.setImageResource(R.drawable.baseball_image)
+            //ラベルをTextViewに反映する
+            profileLabelText.text = "スポーツ"
+            //コメントをTextViewに反映する
+            profileCommentText.text = "好きな動物はネコです！"
+        }
+
+        //スポーツのボタンがタップされた時
+        button3.setOnClickListener{
+
+            //画像をimageViewに反映する
+            profileImage.setImageResource(R.drawable.donut_image)
+            //ラベルをTextViewに反映する
+            profileLabelText.text = "好きな食べ物"
+            //コメントをTextViewに反映する
+            profileCommentText.text = "甘いものが好きです"
+        }
+
+        //スポーツのボタンがタップされた時
+        button4.setOnClickListener{
+
+            //画像をimageViewに反映する
+            profileImage.setImageResource(R.drawable.gadget_image)
+            //ラベルをTextViewに反映する
+            profileLabelText.text = "趣味"
+            //コメントをTextViewに反映する
+            profileCommentText.text = "趣味はゲームです"
         }
     }
 }
